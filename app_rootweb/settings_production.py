ff160d49cccabb434947d3e93360cfbe5f1e7441
azizeko29 <azizeko29undip@gmail.com>
2021-04-21 00:59:54 +0700

    set python version

--- app_rootweb/settings_production.py
+++ app_rootweb/settings_production.py
@@ -1,12 +1,13 @@
 from .settings import *
-# import environ
+import dj_database_url
 
-# env = environ.Env()
+import environ
+env = environ.Env()
 
 ALLOWED_HOSTS += ['bismillah-konvergen-ai.herokuapp.com']
 
-SECRET_KEY = config('DJANGO_SECRET_KEY')
-DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+DEBUG = env('DJANGO_DEBUG', default=False, cast=bool)
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
