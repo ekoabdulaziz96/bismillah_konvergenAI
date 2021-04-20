87c1ed40aa67e974fdd2ff5342ee8cbbd10fe959
azizeko29 <azizeko29undip@gmail.com>
2021-04-21 00:52:59 +0700

    set production

--- app_rootweb/settings_production.py
+++ app_rootweb/settings_production.py
@@ -1,1 +1,14 @@
 from .settings import *
+# import environ
+
+# env = environ.Env()
+
+ALLOWED_HOSTS += ['bismillah-konvergen-ai.herokuapp.com']
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- app_rootweb/wsgi.py
+++ app_rootweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_rootweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_rootweb.settings_production')
 
 application = get_wsgi_application()
