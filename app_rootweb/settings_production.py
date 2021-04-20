6a77fff074a21e6e607057bfee52377dafa22ba5
azizeko29 <azizeko29undip@gmail.com>
2021-04-21 01:05:03 +0700

    set env

--- app_rootweb/settings_production.py
+++ app_rootweb/settings_production.py
@@ -1,15 +1,18 @@
 from .settings import *
 import dj_database_url
 
-import environ
-env = environ.Env()
+# import environ
+# env = environ.Env()
+import os
 
 ALLOWED_HOSTS += ['bismillah-konvergen-ai.herokuapp.com']
 
-SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = env('DJANGO_DEBUG', default=False, cast=bool)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
+INSTALLED_APPS += [whitenoise.runserver_nostatic]
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
